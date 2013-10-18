6479448ef49c2191cd3e9ac1b7ecce7f0705b14f
tathata <ryan.lindgren@gmail.com>
2013-10-18 04:00:53 -0400

    clean up spaces and tabs

--- utils.js
+++ utils.js
@@ -1,19 +1,19 @@
 var fs = require('fs')
-	,	path = require('path')
-	, config = require('./config');
+  , path = require('path')
+  , config = require('./config');
 
 
 // helpers/combiners
 function memoize (fn) {
-	var cache = {}, key;
-	return function () {
-		key = arguments[0] + (arguments[1] || '.all');
-		if (cache[key]) {
-			return cache[key];
-		} else {
-			return (cache[key] = fn.apply(this, arguments));
-		}
-	};
+  var cache = {}, key;
+  return function () {
+    key = arguments[0] + (arguments[1] || '.all');
+    if (cache[key]) {
+      return cache[key];
+    } else {
+      return (cache[key] = fn.apply(this, arguments));
+    }
+  };
 }
 
 // method definitions
@@ -23,25 +23,25 @@ function memoize (fn) {
 // ----------
 var fetchFiles = function (basePath, extname) {
 
-	var nFiles = [];
+  var nFiles = [];
 
-	if (extname && extname[0] !== '.') {
-		extname = '.' + extname;
-	}
+  if (extname && extname[0] !== '.') {
+    extname = '.' + extname;
+  }
 
-	(function loop (fpath) {
-		fs.readdirSync(fpath).forEach(function (p) {
-			pp = path.join(fpath, p)
-			var extMatch = extname ? path.extname(p) === extname : true
-			if (fs.lstatSync(pp).isDirectory()) {
-				loop(pp);
-			} else if ((p[0] !== '.' || p[1] !== '.') && extMatch) {
-				nFiles.push(pp);
-			}
-		});
-	})(basePath);
+  (function loop (fpath) {
+    fs.readdirSync(fpath).forEach(function (p) {
+      pp = path.join(fpath, p)
+      var extMatch = extname ? path.extname(p) === extname : true
+      if (fs.lstatSync(pp).isDirectory()) {
+        loop(pp);
+      } else if ((p[0] !== '.' || p[1] !== '.') && extMatch) {
+        nFiles.push(pp);
+      }
+    });
+  })(basePath);
 
-	return nFiles;
+  return nFiles;
 };
 
 
